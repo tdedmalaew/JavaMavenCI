93101c2e51ca27c0cde529e2a8a129a73d9fc856
tdedmalaew <nuttawut.c39@gmail.com>
2018-10-04 14:20:34 +0700

    commit

---
+++ src/main/java/com/main/Buffet.java
@@ -0,0 +1,15 @@
+package com.main;
+
+public class Buffet {
+	public int number;
+	public int total;
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
+	
+}
---
+++ src/test/java/com/test/BuffetTest.java
@@ -0,0 +1,47 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.main.Buffet;
+
+import junit.framework.Assert;
+
+public class BuffetTest {
+	
+	public int number;
+	public Buffet buf;
+	
+	@Before
+	public void before() {
+		this.number = 0;
+		this.buf = new Buffet();
+	}
+
+	@Test
+	public void getNumberFromSetNumberByNumber() {
+		this.number = 5;
+		
+		int expected = 5;
+		
+		buf.setNumber(this.number);
+		int actual = buf.getNumber();
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getNumberFromSetNumberByNumber3() {
+		this.number = 3;
+		
+		int expected = 3;
+		
+		buf.setNumber(this.number);
+		int actual = buf.getNumber();
+		
+		Assert.assertEquals(expected, actual);
+	}
+
+}
