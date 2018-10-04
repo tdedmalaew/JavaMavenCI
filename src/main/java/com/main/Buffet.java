a6afb44981eca067b0db28af5349a6174a8bcd93
tdedmalaew <nuttawut.c38@gmail.com>
2018-10-05 02:48:53 +0700

    src,pom

--- src/main/java/com/main/Buffet.java
+++ src/main/java/com/main/Buffet.java
@@ -12,4 +12,14 @@ public class Buffet {
 		return this.number;
 	}
 	
+	public int calculateTotal() {
+		if(this.number > 4) {
+			this.total = (((this.number/5)*3)+(this.number%5))*630;
+		}
+		else {
+			this.total = this.number*630;
+		}
+		return this.total;
+	}
+	
 }
--- src/test/java/com/test/BuffetTest.java
+++ src/test/java/com/test/BuffetTest.java
@@ -43,5 +43,41 @@ public class BuffetTest {
 		
 		Assert.assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void getTotalFromSetTotalByTotal1() {
+		this.number = 1;
+		
+		int expected = 630;
+		
+		buf.setNumber(this.number);
+		int actual = buf.calculateTotal();
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getTotalFromSetTotalByTotal5() {
+		this.number = 5;
+		
+		int expected = 630*3;
+		
+		buf.setNumber(this.number);
+		int actual = buf.calculateTotal();
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getTotalFromSetTotalByTotal20() {
+		this.number = 20;
+		
+		int expected = 630*12;
+		
+		buf.setNumber(this.number);
+		int actual = buf.calculateTotal();
+		
+		Assert.assertEquals(expected, actual);
+	}
 
 }
