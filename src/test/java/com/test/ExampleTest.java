165dde531597ffe77fbfb652570679a448d4e2f7
tdedmalaew <nuttawut.c39@gmail.com>
2018-10-04 13:59:07 +0700

    commit

---
+++ src/test/java/com/test/ExampleTest.java
@@ -0,0 +1,24 @@
+package com.test;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		//init value
+		int score = 80;
+		
+		//expected
+		String expected = "A";
+		
+		//actual
+		String actual = "A";
+		
+		//test
+		Assert.assertEquals(expected, actual);
+	}
+
+}
